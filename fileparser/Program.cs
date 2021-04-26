0282d74b257466f168a68557f205741681df954c
harrypotter2021 <newalexey2020@gmail.com>
2021-04-27 08:21:47 +0900

    input is updated

--- fileparser/Program.cs
+++ fileparser/Program.cs
@@ -18,156 +18,166 @@ namespace fileparser
             int[] date_set_id = {400,1,499,1};// 400-record, 1-field
             int[] number_set_id = { 430,5, 499,3}; // 430-record, 5-filed 
             bool check_flag = false;
-
-            StreamReader sr = File.OpenText(fileurl);
-            string str = sr.ReadToEnd();
-            sr.Close();
-            //string str = File.ReadAllText(fileurl);
-            string[] str_lines = str.Split('`');
-
             int block_count = 0;
-            for (int i = 0; i < str_lines.Length; i++)
-                if (str_lines[i].Contains("200~"))
-                    block_count++;
-
-            string[, ,] data = new string[block_count, 900, 100];
-            int[,] repeat_cnt = new int[block_count,900];       
+            int block_id = 0;
+            bool first_flag = true;
+            string[,] data = new string[900, 100];
+            int[] repeat_cnt = new int[900];
             ////// initialize array//////
-            for (int i = 0; i < block_count; i++)
-                for (int j = 0; j < 900; j++)
-                {
-                    repeat_cnt[i, j] = 0;
-                    for (int k = 0; k < 100; k++)
-                        data[i, j, k] = "";
-                }
-                    
-
-            /////////////store data into array//////////
-            int block_id = 0; bool first_flag = true;
-            for (int k = 0; k < str_lines.Length; k++)
+     
+            for (int j = 0; j < 900; j++)
             {
-                if (str_lines[k].Contains('~'))
-                {
-                    str_lines[k] = str_lines[k].Replace("\r\n","");
-                    string[] subdata = str_lines[k].Split('~');
-                    int record_id = int.Parse(subdata[0]);
-
-                    if (record_id == 200)
-                    {
-                        if (first_flag == true)
-                            first_flag = false;
-                        else
-                            block_id++;                        
-                    }
-                    if (record_id != 899)
-                    {                       
-                        data[block_id, record_id, 0] = subdata[0];
-                        repeat_cnt[block_id, record_id] ++;                      
-
-                        for (int j = 1; j < subdata.Length; j++)
-                        {
-                            int field_id = int.Parse(subdata[j].Substring(0, 2));
-                            string value = subdata[j].Substring(2);
-
-                            if (record_id >= 100 && record_id < 200)
-                                data[0, record_id, field_id] = value;
-                            else
-                            {
-                                check_flag = false;
-                                for (int t = 0; t < date_set_id.Length / 2; t++)
-                                {
-                                    if (record_id == date_set_id[t * 2] && field_id == date_set_id[t * 2 + 1])
-                                    {
-                                        string date = value;
-                                        date = date.Substring(0, 2) + "/" + date.Substring(2, 2) + "/" + date.Substring(4);
-                                        data[block_id, record_id, field_id] += repeat_cnt[block_id, record_id] + ";" + date + ";";
-                                        check_flag = true; break;
-                                    }
-                                }
-                                for (int t = 0; t < number_set_id.Length / 2; t++)
-                                {
-                                    if (record_id == number_set_id[t * 2] && field_id == number_set_id[t * 2 + 1])
-                                    {
-                                        double num_val;
-                                        if (double.TryParse(value, out num_val)) // positive number
-                                        {
-                                            if (num_val / 100 < 1000)
-                                                data[block_id, record_id, field_id] += repeat_cnt[block_id, record_id] + ";" + string.Format("{0:0.00}", num_val / 100) + ";";
-                                            else
-                                                data[block_id, record_id, field_id] += repeat_cnt[block_id, record_id] + ";" + string.Format("{0:0,000.00}", num_val / 100) + ";";
-                                        }
-                                        else if (double.TryParse(value.Substring(0, value.Length - 1), out num_val) && value[value.Length - 1] == '-') //negative number
-                                        {
-                                            if (num_val / 100 < 1000)
-                                                data[block_id, record_id, field_id] += repeat_cnt[block_id, record_id] + ";" + string.Format("{0:0.00}", (-1) * num_val / 100) + ";";
-                                            else
-                                                data[block_id, record_id, field_id] += repeat_cnt[block_id, record_id] + ";" + string.Format("{0:0,000.00}", (-1) * num_val / 100) + ";";
-                                          
-                                        }
-                                        check_flag = true; break;
-                                    }
-                                }
-                                if (check_flag == false)
-                                    data[block_id, record_id, field_id] += repeat_cnt[block_id, record_id] + ";" + value + ";";
-                            }
-                        }
-                    }
-                }
+                repeat_cnt[j] = 0;
+                for (int k = 0; k < 100; k++)
+                    data[j, k] = "";
             }
 
-            /////////////print data//////////   
-            for (int i= 0; i< block_count; i++)
+            StreamReader sr = File.OpenText(fileurl);
+            string str = "";
+            string line;
+           
+            while ((line = sr.ReadLine()) != null)
             {
-                Console.WriteLine("-----------------Block"+ (i+1).ToString()+"-------------------------");   
-                if (i == 0)
+              
+                if (line.Contains("200~"))
                 {
-                    for (int j = 100; j < 200; j++)
+                    block_count++;
+                    string[] str_lines = str.Split('`');
+                    str = "";
+                    /////////////store data into array//////////
+                   
+                    for (int k = 0; k < str_lines.Length; k++)
                     {
-                        if (data[i, j, 0] != "")
+                        if (str_lines[k].Contains('~'))
                         {
-                            string temp = "";
-                            for (int k = 1; k < 100; k++)
-                                if (data[i, j, k] != "")
-                                {
-                                    temp = "Block:" + (i + 1).ToString() + " Record :" + data[i, j, 0] + "  Field:" + string.Format("{0:00}", k) + " val: " + data[i, j, k];
-                                    Console.WriteLine(temp);
-                                }
-                            Console.WriteLine("\n");
-                        }
-                    }
-                }      
-                for (int j = 200; j < 899; j++)
-                {
-                    if (data[i, j, 0] != "")
-                    {
-                        for (int cnt =1;cnt<= repeat_cnt[i, j]; cnt++)
-                        {  
-                            string temp = "";
-                            for (int k = 1; k < 100; k++)
-                            {
-                                if (data[i, j, k] != "")
-                                {
-                                    string[] each_item = data[i, j, k].Split(';');
+                            str_lines[k] = str_lines[k].Replace("\r\n", "");
+                            string[] subdata = str_lines[k].Split('~');
+                            int record_id = int.Parse(subdata[0]);
 
-                                   
-                                        for (int ii = 0; ii < (each_item.Length-1)/2; ii++)
+                            if (record_id == 200)
+                            {
+                                if (first_flag == false)
+                                    block_id++;
+                            }
+                            if (record_id != 899)
+                            {
+                                data[ record_id, 0] = subdata[0];
+                                repeat_cnt[record_id]++;
+
+                                for (int j = 1; j < subdata.Length; j++)
+                                {
+                                    int field_id = int.Parse(subdata[j].Substring(0, 2));
+                                    string value = subdata[j].Substring(2);
+
+                                    if (record_id >= 100 && record_id < 200)
+                                        data[record_id, field_id] = value;
+                                    else
+                                    {
+                                        check_flag = false;
+                                        for (int t = 0; t < date_set_id.Length / 2; t++)
                                         {
-                                            if (int.Parse(each_item[2 * ii]) == cnt)
+                                            if (record_id == date_set_id[t * 2] && field_id == date_set_id[t * 2 + 1])
                                             {
-                                                temp = "Block:" + (i + 1).ToString() + " Record :" + data[i, j, 0] + "  Field:" + string.Format("{0:00}", k) + " val: " + each_item[ii*2 +1];
-                                                Console.WriteLine(temp);
+                                                string date = value;
+                                                date = date.Substring(0, 2) + "/" + date.Substring(2, 2) + "/" + date.Substring(4);
+                                                data[record_id, field_id] += repeat_cnt[record_id] + ";" + date + ";";
+                                                check_flag = true; break;
                                             }
                                         }
+                                        for (int t = 0; t < number_set_id.Length / 2; t++)
+                                        {
+                                            if (record_id == number_set_id[t * 2] && field_id == number_set_id[t * 2 + 1])
+                                            {
+                                                double num_val;
+                                                if (double.TryParse(value, out num_val)) // positive number
+                                                {
+                                                    if (num_val / 100 < 1000)
+                                                        data[record_id, field_id] += repeat_cnt[record_id] + ";" + string.Format("{0:0.00}", num_val / 100) + ";";
+                                                    else
+                                                        data[record_id, field_id] += repeat_cnt[record_id] + ";" + string.Format("{0:0,000.00}", num_val / 100) + ";";
+                                                }
+                                                else if (double.TryParse(value.Substring(0, value.Length - 1), out num_val) && value[value.Length - 1] == '-') //negative number
+                                                {
+                                                    if (num_val / 100 < 1000)
+                                                        data[record_id, field_id] += repeat_cnt[record_id] + ";" + string.Format("{0:0.00}", (-1) * num_val / 100) + ";";
+                                                    else
+                                                        data[record_id, field_id] += repeat_cnt[record_id] + ";" + string.Format("{0:0,000.00}", (-1) * num_val / 100) + ";";
+
+                                                }
+                                                check_flag = true; break;
+                                            }
+                                        }
+                                        if (check_flag == false)
+                                            data[record_id, field_id] += repeat_cnt[record_id] + ";" + value + ";";
+                                    }
                                 }
                             }
-
-                            Console.WriteLine("\n");
                         }
-                    
-                    }  
+                    }
+
+                    /////////////print data//////////   
+          
+                    if (first_flag == true)
+                    {
+                        first_flag = false;
+                        for (int j = 100; j < 200; j++)
+                        {
+                            if (data[j, 0] != "")
+                            {
+                                string temp = "";
+                                for (int k = 1; k < 100; k++)
+                                    if (data[j, k] != "")
+                                    {
+                                        temp = "Block:" + (block_id).ToString() + " Record :" + data[j, 0] + "  Field:" + string.Format("{0:00}", k) + " val: " + data[j, k];
+                                        Console.WriteLine(temp);
+                                    }
+                                Console.WriteLine("\n");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("-----------------Block" + (block_id).ToString() + "-------------------------");
+                        for (int j = 200; j < 899; j++)
+                        {
+                            if (data[j, 0] != "")
+                            {
+                                for (int cnt = 1; cnt <= repeat_cnt[j]; cnt++)
+                                {
+                                    string temp = "";
+                                    for (int k = 1; k < 100; k++)
+                                    {
+                                        if (data[j, k] != "")
+                                        {
+                                            string[] each_item = data[j, k].Split(';');
+
+
+                                            for (int ii = 0; ii < (each_item.Length - 1) / 2; ii++)
+                                            {
+                                                if (int.Parse(each_item[2 * ii]) == cnt)
+                                                {
+                                                    temp = "Block:" + (block_id).ToString() + " Record :" + data[j, 0] + "  Field:" + string.Format("{0:00}", k) + " val: " + each_item[ii * 2 + 1];
+                                                    Console.WriteLine(temp);
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    Console.WriteLine("\n");
+                                }
+
+                            }
+                        }
+                        Console.WriteLine("Block:" + (block_id).ToString() + " Record :899 " + " the end of block" + "\n");
+                    }
+                       
                 }
-                Console.WriteLine("Block:" + (i + 1).ToString()  +" Record :899 " + " the end of block" + "\n"); 
-            }              
+
+                str += line;
+
+            }
+                      
+            sr.Close();          
             Console.ReadLine();    
         }
     }
